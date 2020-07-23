update: fix softmax

validate.py
@@ -174,8 +174,9 @@ def process_final(cfg):
         detail = []
 
         if cfg.dataset.question_type in ['action', 'transition']:
-            sm = torch.nn.Softmax()
-            probs = sm(logits.t(), dim=0) 
+            sm = torch.nn.Softmax(dim=0)
+            print('origin_value::', logits.t())
+            probs = sm(logits.t()) 
             print('>>>> Probs: ', type(probs), probs.size(), probs)
             detail = probs.numpy().tolist()
 